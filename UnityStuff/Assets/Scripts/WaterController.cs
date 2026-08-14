5507d16e939ff4139b6fdd62a535a816dc20e4d1
Mark <markshim674@gmail.com>
2026-08-15 01:57:58 +1000

    Made a variable simple water that we can scale based on provided volume

---
+++ UnityStuff/Assets/Scripts/WaterController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class WaterController : MonoBehaviour
+    {
+        public float givenVolume = 90;
+        public float hundredMil = 0.03f;
+
+        void Update()
+        {
+            if (givenVolume>0) {
+                
+                float actualScale = givenVolume*hundredMil / 100;
+
+                Vector3 currentScale = gameObject.transform.localScale;
+                currentScale.y = actualScale;
+                gameObject.transform.localScale = currentScale;
+
+                float halfHeight = 1f;
+                Vector3 pos = transform.localPosition;
+                pos.y = actualScale * halfHeight;
+                transform.localPosition = pos;
+            }
+        }
+    }
+}
